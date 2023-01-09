93e9cc663913e185a14dad6962606666398455a7
Todd Kridel <todd.kridel@gmail.com>
2023-01-08 18:33:02 -0700

    more futzing with network configuration

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -25,12 +25,15 @@ const hide = (elem) => {
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
-const getNotes = () => fetch("/api/notes", {
+const getNotes = () => {
+ console.log("getNotes call");
+ fetch("/api/notes", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
   });
+}
 
 const saveNote = (note) =>
   fetch("/api/notes", {
@@ -170,7 +173,10 @@ const renderNoteList = async (notes) => {
 };
 
 // Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => getNotes().then(renderNoteList);
+const getAndRenderNotes = () => {
+  console.log("page load: getAndRenderNotes call");
+  getNotes().then(renderNoteList);
+}
 
 if (window.location.pathname === "/notes") {
   saveNoteBtn.addEventListener("click", handleNoteSave);
